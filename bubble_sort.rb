3e414da4119d0a5264a2af2e00bc7294b2475953
Patrick Nyatindo <nyatindopatrick@gmail.com>
2020-03-05 16:18:01 +0300

    fix linting errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,29 +1,28 @@
 def bubble_sort(arr)
-    loop do
-        swapped = false
-        (arr.length - 1).times do |i|
-            if arr[i] > arr[i+1]
-                arr[i],arr[i+1] = arr[i+1],arr[i]
-                swapped = true
-            end
-        end
-        break if swapped == false
+  loop do
+    swapped = false
+    (arr.length - 1).times do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
     end
-    print arr
+    break if swapped == false
+  end
+  print arr
 end
 
-
 def bubble_sort_by(array)
-    loop do
-        swapped = false
-        (array.length-1).times do |j|
-            my_method = yield(array[j], array[j+1])
-            if my_method.positive?
-                array[j], array[j+1] = array[j+1], array[j]
-                swapped = true
-            end
-        end
-        break if swapped == false
+  loop do
+    swapped = false
+    (array.length - 1).times do |j|
+      my_method = yield(array[j], array[j + 1])
+      if my_method.positive?
+        array[j], array[j + 1] = array[j + 1], array[j]
+        swapped = true
+      end
     end
-    array
+    break if swapped == false
+  end
+  array
 end
